b9a028983696450043f4e60fdf740d42b9fa27b1
Lyell Read <lyellread@gmail.com>
2018-10-17 10:53:44 -0700

    Lab 4

---
+++ vm-data/project3/project3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int display_equation(int eq_type, int range_low, int range_high){
+	//print prob to scrn
+	
+	//return ans
+
+
+}
+
+int input_check(int acceptable_min, int acceptable_max){
+
+
+
+
+
+	
+
+}
+
+
+void display_floor (int current_floor, int max_floor){
+	
+
+	for (int index = max_floor; index>0; --index){
+
+
+		if (index == current_floor){
+
+			cout << "|```[X]```|\n";
+		}
+		
+		else{
+			cout << "|`````````|\n";
+
+		}
+	}	
+	
+	cout << "'`````````'" << endl;
+}
+
+
+int main (){
+	
+	int test;
+	int bake = 9;
+
+	test=int(char(bake));
+
+	cout << test << endl;
+
+}
