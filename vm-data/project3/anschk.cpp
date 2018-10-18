221dc43b61c0b775fa77a7357d142052bf114e36
Lyell Read <lyellread@gmail.com>
2018-10-17 19:44:08 -0700

    Progress on Project 3

--- vm-data/project3/anschk.cpp
+++ vm-data/project3/anschk.cpp
@@ -1,56 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 
 using namespace std;
 
-int display_equation(int eq_type, int range_low, int range_high){
-	//print prob to scrn
+int main () {	
 	
-	//return ans
+	string test = "19203";
+	cout << int (test) << endl;
 
-
-}
-
-int input_check(int acceptable_min, int acceptable_max){
-
-
-
-
-
-	
-
-}
-
-
-void display_floor (int current_floor, int max_floor){
-	
-
-	for (int index = max_floor; index>0; --index){
-
-
-		if (index == current_floor){
-
-			cout << "|```[X]```|\n";
-		}
-		
-		else{
-			cout << "|`````````|\n";
-
-		}
-	}	
-	
-	cout << "'`````````'" << endl;
-}
-
-
-int main (){
-	
-	int test;
-	int bake = 9;
-
-	test=int(char(bake));
-
-	cout << test << endl;
 
 }
